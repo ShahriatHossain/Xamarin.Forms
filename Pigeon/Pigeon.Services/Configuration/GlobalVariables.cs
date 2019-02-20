c33942b49dfb06bfd34c6ff3d8dc7ef2d01fd96f
Shahriat Hossain <somel90@gmail.com>
2019-02-20 11:56:43 +0600

    Update GlobalVariables.cs

--- Pigeon/Pigeon.Services/Configuration/GlobalVariables.cs
+++ Pigeon/Pigeon.Services/Configuration/GlobalVariables.cs
@@ -4,11 +4,11 @@ namespace Pigeon.Services.Configuration
 {
     public static class GlobalVariables
     {
-        public static string ProductionApiUrl = "http://pigeonapi.azurewebsites.net";
+        public static string ProductionApiUrl = "xxxxxxx";
         
         public static string ApiUrl = $"{ProductionApiUrl}/api/v1/";
         public static Uri GetApiUri(string action) => new Uri(ApiUrl + action);
 
-        public static string AzureBlobUrl = "http://implevistabd.blob.core.windows.net/pigeoncontainer/";
+        public static string AzureBlobUrl = "xxxxxx";
     }
 }
